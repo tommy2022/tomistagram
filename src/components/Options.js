4fe6a1d89364a194b9ce1bce9b795992a976cd39
Xiangyu Qin <qinx@umich.edu>
2021-12-25 18:40:28 -0500

    fix root of project

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,17 +3,18 @@ import { Link } from "react-router-dom";
 import { Container, Box, Center, HStack } from "@chakra-ui/react";
 
 export default function Options() {
+  const root = process.env.PUBLIC_URL;
   return (
     <Container maxW="44em">
       <Center>
         <HStack spacing={12}>
-          <Link to="/">
+          <Link to={root + "/"}>
             <Box component="span">Project</Box>
           </Link>
-          <Link to="/experience">
+          <Link to={root + "/experience"}>
             <Box component="span">Experience</Box>
           </Link>
-          <Link to="/other">
+          <Link to={root + "/other"}>
             <Box component="span">Other</Box>
           </Link>
         </HStack>
